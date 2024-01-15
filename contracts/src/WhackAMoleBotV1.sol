67485ae7c701aed8ea3ce4adb522c11cd0e70e08
工藤权一 <115307551@qq.com>
2024-01-15 16:34:31 +0800

    self wsl Nmp

--- contracts/src/WhackAMoleBotV1.sol
+++ contracts/src/WhackAMoleBotV1.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.9;
 
 import "openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
-import "openzeppelin-contracts/contracts/token/erc20/IERC20.sol";
-import "openzeppelin-contracts/contracts/token/erc20/utils/SafeERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
 
 import "./protocols/uniswap/IUniswapV2Router.sol";
 import "./protocols/uniswap/IUniswapV3SwapRouter.sol";
